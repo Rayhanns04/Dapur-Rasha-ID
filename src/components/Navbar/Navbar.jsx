aae9a22381abc8fa3fb8208d8fe244906aff5892
rayhanns <muhrayhanns04@gmail.com>
2020-12-30 19:28:18 +0700

    Bug

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,17 @@ import styled from "styled-components";
 import Burger from "./Burger";
 import { Logo, MapPin } from "../../constants/Icons";
 import "../../Styling/NonResponsive.css";
+import { useState } from "react";
 
 const Nav = styled.nav`
   width: 100%;
   height: 25px;
-  ${"" /* padding: 0 20px; */}
   display: flex;
   justify-content: space-between;
   align-items: center;
   z-index: 100;
+  position: sticky;
+  padding: 20px 10px;
 `;
 
 const Left = styled.div`
@@ -24,8 +26,19 @@ const Left = styled.div`
 `;
 
 const Navbar = () => {
+  const [navbarSticky, setNavbarSticky] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setNavbarSticky(true);
+    } else {
+      setNavbarSticky(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
   return (
-    <Nav>
+    <Nav className={navbarSticky ? "navbar active" : "navbar"}>
       <Left className="left">
         <img src={Logo} alt="Logo" />
         <h1 className="logo">DAPUR</h1>
